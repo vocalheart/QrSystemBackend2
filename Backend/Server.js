3518a0817b19b563bd8736c631d0c0024a211a6a
Vocal Heart Infotech Pvt. Ltd. <vocalheart.tech@gmail.com>
2025-08-24 19:59:32 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -72,6 +72,7 @@ app.use((err, req, res, next) => {
 // Start server
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running at http://0.0.0.0:${PORT}`);
+  console.log(`Server is running at http://0.0.0.0:${port}`);
 });
+
 
